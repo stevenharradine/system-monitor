14491d9eb8bdbdd372a111edaee764a6111425d2
Steven Harradine <stevenharradine@gmail.com>
2017-04-10 19:45:08 -0400

    add view all disks

--- server/functionality.js
+++ server/functionality.js
@@ -28,7 +28,7 @@ function updatePage () {
             table += "<th>Hostname</th>"
             table += "<th>CPU Load</th>"
             table += "<th>RAM</th>"
-            table += "<th>Partitions</th>"
+            table += "<th width='400px'>Partitions</th>"
             table += "<th>Last updated</th>"
             table += "</tr>"
 
@@ -37,7 +37,13 @@ function updatePage () {
             table += "<td>" + hostname + "</td>"
             table += "<td><canvas id='" + hostname + "-cpu' data-units='CPU Load' data-type='radial-gauge' data-value='" + cpuLoad + "'></canvas></td>"
             table += "<td><canvas id='" + hostname + "-ram'></canvas></td>"
-            table += "<td><canvas id='" + hostname + "-disk'></canvas></td>"
+            table += "<td class='partitions'>"
+            var partitionCounter = 0
+for (partition in data[hostname]["partitions"]) {
+            table += "<canvas id='" + hostname + "-disk" + partitionCounter + "'></canvas>"
+            partitionCounter++
+}
+            table += "</td>"
             table += "<td>" + data[hostname]["time"] + "</td>"
             table += "</tr>"
         }
@@ -50,153 +56,168 @@ function updatePage () {
             var width = 200
             var height = 200
 
-            var cpuLoad = (data[hostname]["cpuLoadAverage"] / data[hostname]["numberOfProcessors"]) * 100
-            var cpuRadial = new RadialGauge({
-                renderTo: hostname + '-cpu',
-                width: width,
-                height: height,
-                units: '%',
-                title: "CPU Load",
-                value: cpuLoad,
-                minValue: 0,
-                maxValue: 100,
-                majorTicks: [
-                    '0',
-                    '20',
-                    '40',
-                    '60',
-                    '80',
-                    '100'
-                ],
-                minorTicks: 2,
-                valueBox: true
-            })
-            cpuRadial.draw()
-            guages.push(cpuRadial);
-
-            var ramUnits = "KB" // default units for free command
-            var ramTotal = data[hostname]["memoryTotal"]
-            var ramUsed = data[hostname]["memoryUsed"]
-
-            if (ramTotal / 1024 > 1) {
-                ramTotal /= 1024
-                ramUnits = "MB"
-            }
-            if (ramTotal / 1024 > 1) {
-                ramTotal /= 1024
-                ramUnits = "GB"
-            }
-            if (ramTotal / 1024 > 1) {
-                ramTotal /= 1024
-                ramUnits = "TB"
+            try {
+                var cpuLoad = (data[hostname]["cpuLoadAverage"] / data[hostname]["numberOfProcessors"]) * 100
+                var cpuRadial = new RadialGauge({
+                    renderTo: hostname + '-cpu',
+                    width: width,
+                    height: height,
+                    units: '%',
+                    title: "CPU Load",
+                    value: cpuLoad,
+                    minValue: 0,
+                    maxValue: 100,
+                    majorTicks: [
+                        '0',
+                        '20',
+                        '40',
+                        '60',
+                        '80',
+                        '100'
+                    ],
+                    minorTicks: 2,
+                    valueBox: true
+                })
+                cpuRadial.draw()
+                guages.push(cpuRadial);
+            } catch (e) {
+                console.log ("CPU Radial Guage failure: " + e)
             }
 
-            if (ramUnits == "MB") {
-                ramUsed /= 1024
-            } else if (ramUnits == "GB") {
-                ramUsed /= 1024
-                ramUsed /= 1024
-            } else if (ramUnits == "TB") {
-                ramUsed /= 1024
-                ramUsed /= 1024
-                ramUsed /= 1024
+            try {
+                var ramUnits = "KB" // default units for free command
+                var ramTotal = data[hostname]["memoryTotal"]
+                var ramUsed = data[hostname]["memoryUsed"]
+
+                if (ramTotal / 1024 > 1) {
+                    ramTotal /= 1024
+                    ramUnits = "MB"
+                }
+                if (ramTotal / 1024 > 1) {
+                    ramTotal /= 1024
+                    ramUnits = "GB"
+                }
+                if (ramTotal / 1024 > 1) {
+                    ramTotal /= 1024
+                    ramUnits = "TB"
+                }
+
+                if (ramUnits == "MB") {
+                    ramUsed /= 1024
+                } else if (ramUnits == "GB") {
+                    ramUsed /= 1024
+                    ramUsed /= 1024
+                } else if (ramUnits == "TB") {
+                    ramUsed /= 1024
+                    ramUsed /= 1024
+                    ramUsed /= 1024
+                }
+
+                ramSteps = []
+                ramSteps[0] = 0
+                ramSteps[1] = round ((ramTotal/5) * 1, 2)
+                ramSteps[2] = round ((ramTotal/5) * 2, 2)
+                ramSteps[3] = round ((ramTotal/5) * 3, 2)
+                ramSteps[4] = round ((ramTotal/5) * 4, 2)
+                ramSteps[5] = round ((ramTotal/5) * 5, 2)
+
+                var ramRadial = new RadialGauge({
+                    renderTo: hostname + '-ram',
+                    width: width,
+                    height: height,
+                    units: ramUnits,
+                    title: "RAM",
+                    value: ramUsed,
+                    minValue: 0,
+                    maxValue: ramTotal,
+                    majorTicks: ramSteps,
+                    highlights: [
+                        { from: 0, to: ramTotal*.3, color: 'rgba(0,0,0,0)' },
+                        { from: ramTotal*.3, to: ramTotal*.65, color: 'rgba(0,0,0,.05)' },
+                        { from: ramTotal*.65, to: ramTotal*.85, color: 'rgba(0,0,0,.2)' },
+                        { from: ramTotal*.85, to: ramTotal, color: 'rgba(0,0,0,.4)' }
+                    ],
+                    minorTicks: 2,
+                    valueBox: true
+                })
+                ramRadial.draw()
+                guages.push(ramRadial);
+            } catch (e) {
+                console.log ("RAM Radial Guage failure: " + e)
             }
 
-            ramSteps = []
-            ramSteps[0] = 0
-            ramSteps[1] = round ((ramTotal/5) * 1, 2)
-            ramSteps[2] = round ((ramTotal/5) * 2, 2)
-            ramSteps[3] = round ((ramTotal/5) * 3, 2)
-            ramSteps[4] = round ((ramTotal/5) * 4, 2)
-            ramSteps[5] = round ((ramTotal/5) * 5, 2)
+            try {
+                var partitionIndex = 0
+                for (partition in data[hostname]["partitions"]) {
+                    var mountAvailable = data[hostname]["partitions"][partition]["mountAvailable"]
+                    var mountUsed = data[hostname]["partitions"][partition]["mountUsed"]
+                    var mountTotal = data[hostname]["partitions"][partition]["mountTotal"]
 
-            var ramRadial = new RadialGauge({
-                renderTo: hostname + '-ram',
-                width: width,
-                height: height,
-                units: ramUnits,
-                title: "RAM",
-                value: ramUsed,
-                minValue: 0,
-                maxValue: ramTotal,
-                majorTicks: ramSteps,
-                highlights: [
-                    { from: 0, to: ramTotal*.3, color: 'rgba(0,0,0,0)' },
-                    { from: ramTotal*.3, to: ramTotal*.65, color: 'rgba(0,0,0,.05)' },
-                    { from: ramTotal*.65, to: ramTotal*.85, color: 'rgba(0,0,0,.2)' },
-                    { from: ramTotal*.85, to: ramTotal, color: 'rgba(0,0,0,.4)' }
-                ],
-                minorTicks: 2,
-                valueBox: true
-            })
-            ramRadial.draw()
-            guages.push(ramRadial);
+                    var diskTotal = mountTotal
+                    var diskUsed = mountUsed
+                    var diskUnits = "KB"
+                    if (diskTotal / 1024 > 1) {
+                        diskTotal /= 1024
+                        diskUnits = "MB"
+                    }
+                    if (diskTotal / 1024 > 1) {
+                        diskTotal /= 1024
+                        diskUnits = "GB"
+                    }
+                    if (diskTotal / 1024 > 1) {
+                        diskTotal /= 1024
+                        diskUnits = "TB"
+                    }
 
-            var mountAvailable = data[hostname]["partitions"]["/"]["mountAvailable"]
-            var mountUsed = data[hostname]["partitions"]["/"]["mountUsed"]
-            var mountTotal = data[hostname]["partitions"]["/"]["mountTotal"]
+                    if (diskUnits == "MB") {
+                        diskUsed /= 1024
+                    } else if (diskUnits == "GB") {
+                        diskUsed /= 1024
+                        diskUsed /= 1024
+                    } else if (diskUnits == "TB") {
+                        diskUsed /= 1024
+                        diskUsed /= 1024
+                        diskUsed /= 1024
+                    }
+                    diskSteps = []
+                    diskSteps[0] = 0
+                    diskSteps[1] = round ((diskTotal/5) * 1, 2)
+                    diskSteps[2] = round ((diskTotal/5) * 2, 2)
+                    diskSteps[3] = round ((diskTotal/5) * 3, 2)
+                    diskSteps[4] = round ((diskTotal/5) * 4, 2)
+                    diskSteps[5] = round ((diskTotal/5) * 5, 2)
 
-            var diskTotal = mountTotal
-            var diskUsed = mountUsed
-            var diskUnits = "KB"
-            if (diskTotal / 1024 > 1) {
-                diskTotal /= 1024
-                diskUnits = "MB"
+                    var diskRadial = new LinearGauge({
+                        renderTo: hostname + '-disk' + partitionIndex,
+                        width: width*2,
+                        height: height/2,
+                        units: diskUnits,
+                        title: partition,
+                        value: diskUsed,
+                        minValue: 0,
+                        maxValue: diskTotal,
+                        majorTicks: diskSteps,
+                        highlights: [
+                            { from: 0, to: diskTotal*.50, color: 'rgba(0,0,0,0)' },
+                            { from: diskTotal*.50, to: diskTotal*.75, color: 'rgba(0,0,0,.05)' },
+                            { from: diskTotal*.75, to: diskTotal*.90, color: 'rgba(0,0,0,.2)' },
+                            { from: diskTotal*.90, to: diskTotal, color: 'rgba(0,0,0,.4)' }
+                        ],
+                        minorTicks: 2,
+                        valueBox: true
+                    })
+                    diskRadial.draw()
+                    guages.push(diskRadial);
+                    partitionIndex++;
+                }
+            } catch (e) {
+                console.log ("Disk Radial Guage failure: " + e)
             }
-            if (diskTotal / 1024 > 1) {
-                diskTotal /= 1024
-                diskUnits = "GB"
-            }
-            if (diskTotal / 1024 > 1) {
-                diskTotal /= 1024
-                diskUnits = "TB"
-            }
-
-            if (diskUnits == "MB") {
-                diskUsed /= 1024
-            } else if (diskUnits == "GB") {
-                diskUsed /= 1024
-                diskUsed /= 1024
-            } else if (diskUnits == "TB") {
-                diskUsed /= 1024
-                diskUsed /= 1024
-                diskUsed /= 1024
-            }
-
-            diskSteps = []
-            diskSteps[0] = 0
-            diskSteps[1] = round ((diskTotal/5) * 1, 2)
-            diskSteps[2] = round ((diskTotal/5) * 2, 2)
-            diskSteps[3] = round ((diskTotal/5) * 3, 2)
-            diskSteps[4] = round ((diskTotal/5) * 4, 2)
-            diskSteps[5] = round ((diskTotal/5) * 5, 2)
-
-            var diskRadial = new RadialGauge({
-                renderTo: hostname + '-disk',
-                width: width,
-                height: height,
-                units: diskUnits,
-                title: "/",
-                value: diskUsed,
-                minValue: 0,
-                maxValue: diskTotal,
-                majorTicks: diskSteps,
-                highlights: [
-                    { from: 0, to: diskTotal*.50, color: 'rgba(0,0,0,0)' },
-                    { from: diskTotal*.50, to: diskTotal*.75, color: 'rgba(0,0,0,.05)' },
-                    { from: diskTotal*.75, to: diskTotal*.90, color: 'rgba(0,0,0,.2)' },
-                    { from: diskTotal*.90, to: diskTotal, color: 'rgba(0,0,0,.4)' }
-                ],
-                minorTicks: 2,
-                valueBox: true
-            })
-            diskRadial.draw()
-            guages.push(diskRadial);
         }
 
         setTimeout( function () {
             updatePage ()
-        }, 3000)
+        }, 100000)
     })
 }
 
